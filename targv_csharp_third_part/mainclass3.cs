0d2b9198fbc5861a0b6c526f65b7ccd68e92fe19
ReginaKolesnikova <151017512+ReginaKolesnikova@users.noreply.github.com>
2025-06-10 13:02:47 +0300

    Update mainclass3.cs

--- targv_csharp_third_part/mainclass3.cs
+++ targv_csharp_third_part/mainclass3.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace targv_csharp_third_part
+namespace csharp_targv24.targv_csharp_third_part
 {
     internal class mainclass3
     {
