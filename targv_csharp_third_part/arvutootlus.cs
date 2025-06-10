8b8a630cf8af0b97eab6f4317b1258b495bdca40
ReginaKolesnikova <151017512+ReginaKolesnikova@users.noreply.github.com>
2025-06-10 11:46:11 +0300

    Add files via upload

---
+++ targv_csharp_third_part/arvutootlus.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targv_csharp_third_part
+{
+    internal class arvutootlus
+    {
+        public static (int[], int[]) GenereeriRuudud(int min, int max)
+        {
+          
+            Random rnd = new Random();
+            int N = rnd.Next(min, max);
+            int M = rnd.Next(min, max);
+
+            int väiksem, suurem;
+
+            if (N < M)
+            {
+                väiksem = N;
+                suurem = M;
+            }
+            else
+            {
+                väiksem = M;
+                suurem = N;
+            }
+
+            int[] arvud = new int[suurem - väiksem - 1];
+            int[] ruudud = new int[suurem - väiksem - 1];
+
+            for (int i = 0; i < ruudud.Length; i++)
+            {
+                arvud[i] = väiksem + i + 1; 
+                ruudud[i] = arvud[i] * arvud[i]; 
+            }
+
+            return (ruudud, arvud);
+        }
+    }
+}
---
+++ targv_csharp_third_part/funktsioonideclass3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targv_csharp_third_part
+{
+    internal class funktsioonideclass3
+    {
+        public static List<string> Sõnad()
+        {
+            List<string> sonad = new List<string>();
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write("Nimi: ");
+                sonad.Add(Console.ReadLine());
+            }
+            return sonad;
+        }
+
+        public static Isik[] Isikud(int k, string[] nimed, string[] aadressid)
+        {
+            Isik[] isikud = new Isik[k];
+
+            for (int i = 0; i < k; i++)
+            {
+                Console.WriteLine(i);
+                Console.Write("Isikukood: ");
+                isikud[i] = new Isik
+                {
+                    Nimi = nimed[i],
+                    Vanus = 50,
+                    Isikukood = Console.ReadLine(),
+                    Aadress = aadressid[i]
+                };
+            }
+            return isikud;
+        }
+        public static List<Isik> Isikud2(int k, string[] nimed, string[] aadressid)
+        {
+            List<Isik> isikud2 = new List<Isik>();
+            for (int j = 0; j > k; j++)
+            {
+                Console.WriteLine(j);
+                Isik isik = new Isik
+                {
+                    Nimi = nimed[j],
+                    Vanus = 50,
+                    Isikukood = "111111111111",
+                    Aadress = aadressid[j]
+                };
+                isikud2.Add(isik);
+            }
+            return isikud2;
+        }
+    }
+}
