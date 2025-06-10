071d73622c246aac7505489665e9aae27dbb1c5a
ReginaKolesnikova <151017512+ReginaKolesnikova@users.noreply.github.com>
2025-06-10 12:00:14 +0300

    Add files via upload

---
+++ targv_csharp_fourth_part/mainclass4.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targv_csharp_fourth_part
+{
+    internal class mainclass4
+    {
+        public static void Main(string[] args)
+        {
+            string path;
+
+            // ------- Faili kirjutamine(StreamWriter) -------
+            try
+            {
+                path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"C:\\Kasutajad\\opilane\\source\\repos\\Kolesnikova_TARgv24\\targv_csharp_fourth_part\\kuud.txt"); //@"..\..\..\kuud.txt"
+                using (StreamWriter text = new StreamWriter(path, true))  // Fail suletakse automaatselt siin
+                {
+                    Console.WriteLine("Sisesta mingi tekst: ");
+                    string lause = Console.ReadLine();
+                    text.WriteLine(lause);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Mingi viga failiga");
+            }
+
+
+            // ------ Faili lugemine (StreamReader) --------
+            try
+            {
+                path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"C:\\Kasutajad\\opilane\\source\\repos\\Kolesnikova_TARgv24\\targv_csharp_fourth_part\\kuud.txt");
+                StreamReader text = new StreamReader(path);
+                string laused = text.ReadToEnd();
+                text.Close();
+                Console.WriteLine(laused);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Mingi viga failiga, ei saa faili lugeda");
+            }
+
+
+            // ------ Ridade lugemine List<string> abil -------
+            List<string> kuude_list = new List<string>();
+            try
+            {
+                path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"C:\\Kasutajad\\opilane\\source\\repos\\Kolesnikova_TARgv24\\targv_csharp_fourth_part\\kuud.txt");
+                foreach (string rida in File.ReadAllLines(path))
+                {
+                    kuude_list.Add(rida);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Viga failiga!");
+            }
+
+
+            // ------ Listi muutmine ja kuvamine -------
+            foreach (string kuu in kuude_list)
+            {
+                Console.WriteLine(kuu);
+            }
+
+            // Eemalda "Juuni"
+            kuude_list.Remove("Juuli");
+
+            // Muuda esimest elementi
+            if (kuude_list.Count > 0)
+                kuude_list[0] = "Veel kuu";
+
+            Console.WriteLine("--------------Kustutasime juuli-----------");
+
+            foreach (string kuu in kuude_list)
+            {
+                Console.WriteLine(kuu);
+            }
+
+
+            // ------ Otsing nimekirjast -------
+            Console.WriteLine("Sisesta kuu nimi, mida otsida:");
+            string otsitav = Console.ReadLine();
+
+            if (kuude_list.Contains(otsitav))
+                Console.WriteLine("Kuu " + otsitav + " on olemas.");
+            else
+                Console.WriteLine("Sellist kuud pole.");
+
+
+            // -----  Listi salvestamine tagasi faili ------
+            path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "kuud.txt");
+            File.WriteAllLines(path, kuude_list);
+            Console.WriteLine("Andmed on salvestatud.");
+        }
+    }
+}
---
+++ targv_csharp_fourth_part/mainclass_task.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targv_csharp_fourth_part
+{
+    internal class mainclass_task
+    {
+        public static void Main(string[] args)
+        {
+            // 1. Loo failinimi Kuud.txt
+            string path = funktsioonideclass4.FailiPath(@"C:\\Kasutajad\\opilane\\source\\repos\\Kolesnikova_TARgv24\\targv_csharp_fourth_part\\kuud.txt");
+
+            // 2. Kirjuta programmi abil 3 kuud faili
+            funktsioonideclass4.FailiKirjutamine(path);
+
+            // 3. Loe kuud List<string> sisse
+            List<string> kuude_list = funktsioonideclass4.FailiLugemine(path);
+
+            // 4. Eemalda „Juuni“, muuda esimene element
+            funktsioonideclass4.EemaldamineMuutmine(path, kuude_list);
+
+            // 5. Kuvada kõik kuud
+            foreach (string kuu in kuude_list)
+            {
+                Console.WriteLine(kuu);
+            }
+
+            // 6. Luba kasutajal otsida kuud nime järgi
+            Console.WriteLine("Sisesta kuu nimi, mida otsida:");
+            string otsitav_kuu = Console.ReadLine();
+            string vastus = funktsioonideclass4.Otsing(otsitav_kuu, kuude_list);
+            Console.WriteLine(vastus);
+
+            // 7. Salvesta muudatused faili tagasi
+            funktsioonideclass4.Salvestamine(path, kuude_list);
+
+        }
+    }
+}
